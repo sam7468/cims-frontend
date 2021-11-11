05bc56e1d00c3680276d67ec84a4ec71d1062738
sam7468 <samsharan7468@gmail.com>
2021-11-12 00:12:05 +0530

    added MUI-sidebar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-
+import CollapsibleSidebar from './Collapsible-Sidebar'
+import Sidebar from './Sidebar';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {false && <Sidebar/>}
+      <CollapsibleSidebar/>
     </div>
   );
 }
